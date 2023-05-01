28b505d4555f4d9c3f5e4d8c4324bb3edab94a7b
georgianasima <georgiana.sima28@gmail.com>
2023-05-01 00:27:06 -0400

    Testing-Assignment

--- src/main/java/org/example/DistanceCalculator.java
+++ src/main/java/org/example/DistanceCalculator.java
@@ -1,2 +1,51 @@
-package org.example;public class DistanceCalculator {
+package org.example;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DistanceCalculator {
+    public static int computeDistance(String expression, String outputUnit) {
+        // Convert expression to a list of terms
+        List<String> terms=Arrays.asList(expression.split("\\s*[-+]\\s*"));
+
+        // Compute the total value in millimeters
+        int total = 0;
+        for (String term :terms) {
+            int value = Integer.parseInt(term.replaceAll("[^0-9]", ""));
+            String unit = term.replaceAll("[^a-z]", "");
+            switch (unit) {
+                case "mm":
+                    total += value;
+                    break;
+                case "cm":
+                    total += value * 10;
+                    break;
+                case "dm":
+                    total += value * 100;
+                    break;
+                case "m":
+                    total += value * 1000;
+                    break;
+                case "km":
+                    total += value * 1000000;
+                    break;
+                default:
+                    throw new IllegalArgumentException("Invalid unit: " + unit);
+            }
+        }
+        switch (outputUnit) {
+            case "mm":
+                return total;
+            case "cm":
+                return total / 10;
+            case "dm":
+                return total / 100;
+            case "m":
+                return total / 1000;
+            case "km":
+                return total / 1000000;
+            default:
+                throw new IllegalArgumentException("Invalid unit: " + outputUnit);
+        }
+    }
 }
--- src/test/java/org/example/Test.java
+++ src/test/java/org/example/Test.java
@@ -1,2 +1,51 @@
-package org.example;public class Test {
+package org.example;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class DistanceCalculatorTest {
+    @Nested
+    @DisplayName("computeDistance method")
+    class ComputeDistanceTests {
+        @Test
+        @DisplayName("should return the value in millimeters for an expression in millimeters")
+        void testComputeDistanceWithExpressionInMillimeters() {
+            int result = DistanceCalculator.computeDistance("10 mm", "mm");
+            assertEquals(10, result);
+        }
+
+        @Test
+        @DisplayName("should return the value in millimeters for an expression in centimeters")
+        void testComputeDistanceWithExpressionInCentimeters() {
+            int result = DistanceCalculator.computeDistance("10 cm", "mm");
+            assertEquals(100, result);
+        }
+        @Test
+        @DisplayName("should return the value in millimeters for an expression in decimeters")
+        void testComputeDistanceWithExpressionInDecimeters() {
+            int result = DistanceCalculator.computeDistance("10 dm", "mm");
+            assertEquals(1000, result);
+        }
+        @Test
+        @DisplayName("should return the value in millimeters for an expression in meters")
+        void testComputeDistanceWithExpressionInMeters() {
+            int result = DistanceCalculator.computeDistance("10 m", "mm");
+            assertEquals(10000, result);
+        }
+        @Test
+        @DisplayName("should return the value in millimeters for an expression in kilometers")
+        void testComputeDistanceWithExpressionInKilometers() {
+            int result = DistanceCalculator.computeDistance("10 km", "mm");
+            assertEquals(10000000, result);
+        }
+        @DisplayName("should handle complex expressions with different units")
+        void testComputeDistanceWithComplexExpressions(String expression, String outputUnit, int expected) {
+            int result = DistanceCalculator.computeDistance(expression, outputUnit);
+            assertEquals(expected, result);
+        }
+
+    }
+
 }
